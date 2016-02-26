4135634629efdacf0666fb9135b5ff4d3b84a87c
martin_carrizo <carrizofm2010@gmail.com>
2016-02-26 00:09:11 -0300

    Omega con share de facebook

---
+++ Omega Eventos/Assets/script/accesoOmega.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+public class accesoOmega : MonoBehaviour, IVirtualButtonEventHandler {
+
+	// Use this for initialization
+	void Start () {
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for (int i = 0; i < vbs.Length; ++i) {
+			
+			vbs[i].RegisterEventHandler(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+		
+		// si se presiona el boton virtual quebrada
+		if (vb.VirtualButtonName == "VirtualOmega") {
+			
+			Application.OpenURL("https://www.facebook.com/events/558981187604216/");
+			
+		}
+		
+	}
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) { 
+	}
+}
---
+++ Omega Eventos/Assets/script/captureScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class captureScreen : MonoBehaviour {
+	
+	// Use this for initialization
+	public void ButtonScreenShot(){
+		StartCoroutine ("ScreenshotEncode");
+	}
+	
+	private IEnumerator ScreenshotEncode (){
+		yield return new WaitForEndOfFrame ();
+		string datacion = System.DateTime.Now.ToFileTime ().ToString ();
+		Application.CaptureScreenshot (datacion+".png");
+	}
+}
